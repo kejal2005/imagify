7aef25ea5e99b30ece51d9177bfbf5befbb944f2
Kejal Jain <ruchitajainujjain123@gmail.com>
2025-07-06 20:26:49 +0530

    Fix import path case for Steps component in Home.jsx (Vercel build error)

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
-import Steps from '../components/steps';
+import Steps from '../components/Steps';
 import Description from '../components/Description';
 import Testimonial from '../components/Testimonial';
 import GenerateBtn from '../components/GenerateBtn';
